8bdeabe6f4b92b9b752de97b9d9a37dbf48a1805
Thomas Winter <wintert98@gmail.com>
2020-10-23 17:47:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,9 @@
 class Dog
-  def initialize(dogs_name)
+  def name=(dogs_name)
     @dogs_name = dogs_name
   end
   def name
     @dogs_name
   end
-  def name=(new_dogs_name)
-    @dogs_name = new_dogs_name
-  end
+  
 end
